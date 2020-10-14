c5fae4b7f5775af0adc82e46eb903e00ea2ce3bf
Phan Phát Tài <70010376+TaiTitans@users.noreply.github.com>
2020-10-14 07:52:48 +0700

    Add files via upload

---
+++ hackfb.cpp
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include <time.h>
+void wait(int seconds)
+{
+clock_t ew;
+ew = clock() + seconds * CLOCKS_PER_SEC;
+while (clock() < ew) {}
+}
+int main() {
+wait(2);
+printf(" CHAO MUNG BAN DEN VOI HACK FB\n\n\n\n");
+wait(2);
+printf("Dang tai du lieu ...\n");
+wait(2);
+printf("Ket noi sever thanh cong ! \n");
+wait(2);
+printf("Moi ban nhap link Fb can hack: ");
+char n[100];
+scanf("%s", &n);
+printf("Moi ban chon kieu hack: \n");
+wait(0.5);
+printf("1. Hack Check Pass \n2. Hack delete Fb \n");
+wait(2);
+int i;
+do {
+scanf("%d", &i);
+if (i != 1 && i != 2) {
+printf("Ðit me mày nhap cho dung\n");
+}
+if (i == 1) {
+printf("Chung toi dang xu li yeu cau cua ban hack mat khau cua ban \n");
+wait(0.5);
+printf("Loading ... 0 % \n");
+wait(1);
+printf("Loading ... 10 Phan tram \n");
+wait(1);
+printf("Loading ... 20 Phan tram \n");
+wait(1);
+printf("Loading ... 30 Phan tram \n");
+wait(1);
+printf("Loading ... 40 Phan tram \n");
+wait(1);
+printf("Loading ... 50 Phan tram \n");
+wait(1);
+printf("Loading ... 60 Phan tram \n");
+wait(1);
+printf("Loading ... 70 Phan tram \n");
+wait(1);
+printf("Loading ... 80 Phan tram \n");
+wait(1);
+printf("Loading ... 90 Phan tram \n");
+wait(1);
+printf("Loading ... 100 Phan tram \n");
+wait(4);
+printf("Da hack thanh cong vvvv\n");
+wait(5);
+printf("Day la Mk Fb nha ban : taideptraivailon\n");
+printf("\n\n\n Thank for using ! <3 \n");
+}
+if (i == 2) {
+printf("Chúng tôi dang c? lí yêu c?u c?a b?n hack xoá Fb nha ....\n");
+wait(1);
+printf("Loading ... 0 %\n");
+wait(1);
+printf("Loading ... 10 %\n");
+wait(1);
+printf("Loading ... 20 %\n");
+wait(1);
+printf("Loading ... 30 %\n");
+wait(1);
+printf("Loading ... 40 %\n");
+wait(1);
+printf("Loading ... 50 %\n");
+wait(1);
+printf("Loading ... 60 %\n");
+wait(1);
+printf("Loading ... 70 %\n");
+wait(1);
+printf("Loading ... 80 %\n");
+wait(1);
+printf("Loading ... 90 %\n");
+wait(1);
+printf("Loading ... 0 %100\n");
+wait(4);
+printf("Fb nay da bi xoa\n");
+wait(5);
+printf("\n\n\nÐã hack thành công vvvv\n");
+printf(" Thank for using ! ?? \n");
+}
+}
+while (i != 1 && i != 2);
+}
